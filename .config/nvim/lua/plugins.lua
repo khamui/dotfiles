7de765e9f14945f0f3cd5bc6b5d96de66a8b720e
Kha <k.tran@liqd.net>
2022-03-08 22:11:36 +0000

    luasnip configured for nvim-cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,6 +15,11 @@ Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
+Plug 'saadparwaiz1/cmp_luasnip'
+-- snippets
+Plug 'L3MON4D3/LuaSnip'
+Plug 'rafamadriz/friendly-snippets'
+
 -- auto-close plugin
 -- cmd [[Plug 'rstacruz/vim-closer'
 
